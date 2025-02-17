e333b0f203bd6547d73b10ce695007a7f2e42341
rjteche <rjteche@gmail.com>
2025-02-17 15:27:22 +0530

    Update jenkins.sh
    Update jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# USE UBUNTU20.04 - INSTANCE: 2GB RAM + 2VCPU MIN - WILL ONLY WORK
 sudo apt update -y
 sudo apt install openjdk-17-jdk -y
 sudo apt update -y
 sudo apt install maven -y
+sudo apt install git -y
+sudo apt update -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
@@ -13,5 +14,3 @@ sudo apt update -y
 sudo apt install jenkins -y
 service jenkins start
 cat /var/lib/jenkins/secrets/initialAdminPassword
-#chmod 777 jenkins.sh
-#./jenkins.sh
